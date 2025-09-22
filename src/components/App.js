fbc5ebf0430ee23b86aece1fd1ea1c77ec0239b7
aveencoder <aveen598@gmail.com>
2025-09-22 22:03:05 +0530

    sucess

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[selectedOption,setSelectedOption] = useState('');
+  
+  const handleClick=(change)=>{
+      setSelectedOption(change)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1 style={{margin:"50px"}}>Parent Component</h1>
+        <ChildChildern1  onChange={handleClick}/>
+        <ChildChildern2 onChange={handleClick}/>
+        <p>Selected Option: {selectedOption}</p>
     </div>
   )
 }
 
+
+
+function ChildChildern1({onChange}) {
+  return (
+    <div style={{backgroundColor:"sienna",padding:"10px",marginBottom:"10px"}}>
+      <h1>Child Component 1</h1>
+       <button onClick={()=>onChange("Option 1")}>Option 1</button>
+      </div>
+  )
+}
+
+function ChildChildern2({onChange}) {
+  return (
+    <div style={{backgroundColor:"gold",padding:"10px"}}>
+      <h1>Child Component 2</h1>
+       <button onClick={()=>onChange("Option 2")}>Option 2</button>
+      </div>
+  )
+}
+
+
+
+
+
+
 export default App
+
